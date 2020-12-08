6338755154d039694a946eb40c7127db9c1282e8
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-12-08 19:22:12 +1100

    basic controller that prints to screen notes

--- src/main/kotlin/com/learnkotlinbackend/learnkotlinbackend/LearnKotlinBackendApplication.kt
+++ src/main/kotlin/com/learnkotlinbackend/learnkotlinbackend/LearnKotlinBackendApplication.kt
@@ -2,11 +2,22 @@ package com.learnkotlinbackend.learnkotlinbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class LearnKotlinBackendApplication
+open class LearnKotlinBackendApplication {}
+
+// Basic controller
+// @RestController
+// class HelloSpringBootController {
+	// RequestMapping annotation registers that this method as a request handler and maps it to the "/" url.
+	// @RequestMapping(value = ["/"])
+	// Our class method will print "Hello SpringBoot" to the sreen.
+	// fun helloSpringBoot() = "Hello SpringBoot"
+// }
 
 fun main(args: Array<String>) {
 	runApplication<LearnKotlinBackendApplication>(*args)
-	println("hello world")
+//	println("hello world")
 }
