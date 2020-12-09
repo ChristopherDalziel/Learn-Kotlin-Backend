703f76e7b84b71ece715954b2531b6bca16034af
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-12-09 21:08:04 +1100

    duplicateitem class, database error

---
+++ src/main/kotlin/com/learnkotlinbackend/learnkotlinbackend/DuplicateItemException.kt
@@ -0,0 +1,16 @@
+package com.learnkotlinbackend.learnkotlinbackend
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Duplicate item.")
+class DuplicateItemException: RuntimeException() {
+    @RequestMapping("", method = arrayOf(RequestMethod.POST))
+    fun addBook(@RequestBody book: Book) =
+        if (database.addBook(book)) book
+    else throw DuplicateItemException()
+}
